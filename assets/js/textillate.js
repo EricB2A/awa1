72bbf9af77d6fb1c6878999bd270de596c1d4aa0
Eric Broutba <eric.bousbaa@hotmail.com>
2018-01-21 18:25:58 +0100

    text animation

---
+++ assets/js/textillate.js
@@ -0,0 +1,9 @@
+/*
+    https://github.com/jschr/textillate
+*/
+
+$('#header .content .inner > :last-child').textillate({
+    initialDelay: 1500,
+    in: { effect: 'fadeIn' },
+    sync: true
+});
